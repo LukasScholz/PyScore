726a8e6c98dbc65c981b36e55bd3b8217488587e
Lukas Scholz <l.kas.scholz@gmail.com>
2025-12-31 15:56:16 +0100

    Added simple bar detection

--- src/PyScore/main.py
+++ src/PyScore/main.py
@@ -20,15 +20,13 @@ with pdf_tools.PDF2Image(FILEPATH) as files:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-        # Detect horizontal lines
-        kernel_length = 60
-        kernel_width = 1
-        horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, kernel_width))
-        detect_horizontal = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
-        cnts = cv2.findContours(detect_horizontal, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Detect vertical lines
+        vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 20))
+        detect_vertical = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
+        cnts = cv2.findContours(detect_vertical, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if len(cnts) == 2 else cnts[1]
         for c in cnts:
-            cv2.drawContours(result, [c], -1, (36, 255, 12), 2)
+            cv2.drawContours(result, [c], -1, (36, 255, 12), 1)
 
         cv2.imshow('result', result)
         cv2.waitKey()
