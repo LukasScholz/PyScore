06d9347a4b34f15829ef70ee0945a972a0363be0
Lukas Scholz <l.kas.scholz@gmail.com>
2026-01-01 16:40:24 +0100

    removed unused imports

--- src/PyScore/main.py
+++ src/PyScore/main.py
@@ -1,16 +1,7 @@
-import time
-
 import cv2
 import pdf_tools
 
 FILEPATH = "../../examples/moonlight_sonata.pdf"
-
-#with pdf_tools.PDF2Image(FILEPATH) as files:
-#    for f in files:
-#        img = cv2.imread(f, cv2.IMREAD_GRAYSCALE)
-#        cv2.imshow("Score", img)
-#        cv2.waitKey(0)
-#        cv2.destroyAllWindows()
 
 with pdf_tools.PDF2Image(FILEPATH) as files:
     for f in files:
